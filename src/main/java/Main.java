50ddf9150190d7271e192f4bca3d1ad3181f25ff
salvador_sakho <Salvador.Sakho21@gmail.com>
2016-11-08 19:51:55 +0200

    First Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ${BIM} on 08.11.2016.
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("Hello");
+        System.out.println("Repo");
+    }
+}
